f0b7da96339f7d9a29258722ee219fa8e840c515
Efrain Toribio <efrainreyes05@hotmail.com>
2015-06-04 13:50:21 -0400

    CRUD position ready..UI and code..

--- Nomina/PositionForm.Designer.cs
+++ Nomina/PositionForm.Designer.cs
@@ -32,27 +32,31 @@
             this.positionsBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.payrollunapecDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this._payroll_unapecDataSet = new Nomina._payroll_unapecDataSet();
-            this.positionsTableAdapter = new Nomina._payroll_unapecDataSetTableAdapters.PositionsTableAdapter();
             this.label1 = new System.Windows.Forms.Label();
             this.positionName = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.positionRisk = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.positionMinSalary = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
             this.positionMaxSalary = new System.Windows.Forms.TextBox();
-            this.savePosition = new System.Windows.Forms.Button();
-            this.cancelPosition = new System.Windows.Forms.Button();
             this.positionsDataGrid = new System.Windows.Forms.DataGridView();
+            this.cmbRiesgo = new System.Windows.Forms.ComboBox();
+            this.savePosition = new System.Windows.Forms.Button();
+            this.positionsTableAdapter = new Nomina._payroll_unapecDataSetTableAdapters.PositionsTableAdapter();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this._payroll_unapecDataSet1 = new Nomina._payroll_unapecDataSet();
+            this.btnEliminar = new System.Windows.Forms.Button();
             this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.nameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.riskLevelDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.riskLevelDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewComboBoxColumn();
             this.minSalaryDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.maxSalaryDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.positionsBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.payrollunapecDataSetBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this._payroll_unapecDataSet)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.positionsDataGrid)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this._payroll_unapecDataSet1)).BeginInit();
             this.SuspendLayout();
             // 
             // positionsBindingSource
@@ -70,55 +74,43 @@
             this._payroll_unapecDataSet.DataSetName = "_payroll_unapecDataSet";
             this._payroll_unapecDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
             // 
-            // positionsTableAdapter
-            // 
-            this.positionsTableAdapter.ClearBeforeFill = true;
-            // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(12, 35);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.Size = new System.Drawing.Size(47, 13);
             this.label1.TabIndex = 1;
-            this.label1.Text = "Nombre";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.label1.Text = "Nombre:";
             // 
             // positionName
             // 
-            this.positionName.Location = new System.Drawing.Point(65, 32);
+            this.positionName.Location = new System.Drawing.Point(104, 34);
             this.positionName.Name = "positionName";
-            this.positionName.Size = new System.Drawing.Size(100, 20);
+            this.positionName.Size = new System.Drawing.Size(121, 20);
             this.positionName.TabIndex = 2;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 69);
+            this.label2.Location = new System.Drawing.Point(12, 76);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.Size = new System.Drawing.Size(85, 13);
             this.label2.TabIndex = 3;
-            this.label2.Text = "Riesgo";
-            // 
-            // positionRisk
-            // 
-            this.positionRisk.Location = new System.Drawing.Point(65, 66);
-            this.positionRisk.Name = "positionRisk";
-            this.positionRisk.Size = new System.Drawing.Size(100, 20);
-            this.positionRisk.TabIndex = 4;
+            this.label2.Text = "Nivel de Riesgo:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(208, 34);
+            this.label3.Location = new System.Drawing.Point(251, 34);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(59, 13);
+            this.label3.Size = new System.Drawing.Size(62, 13);
             this.label3.TabIndex = 5;
-            this.label3.Text = "Salario Min";
+            this.label3.Text = "Salario Min:";
             // 
             // positionMinSalary
             // 
-            this.positionMinSalary.Location = new System.Drawing.Point(277, 31);
+            this.positionMinSalary.Location = new System.Drawing.Point(331, 31);
             this.positionMinSalary.Name = "positionMinSalary";
             this.positionMinSalary.Size = new System.Drawing.Size(100, 20);
             this.positionMinSalary.TabIndex = 6;
@@ -126,40 +118,23 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(208, 68);
+            this.label4.Location = new System.Drawing.Point(251, 73);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(62, 13);
+            this.label4.Size = new System.Drawing.Size(65, 13);
             this.label4.TabIndex = 7;
-            this.label4.Text = "Salario Max";
+            this.label4.Text = "Salario Max:";
             // 
             // positionMaxSalary
             // 
-            this.positionMaxSalary.Location = new System.Drawing.Point(277, 66);
+            this.positionMaxSalary.Location = new System.Drawing.Point(331, 70);
             this.positionMaxSalary.Name = "positionMaxSalary";
             this.positionMaxSalary.Size = new System.Drawing.Size(100, 20);
             this.positionMaxSalary.TabIndex = 8;
             // 
-            // savePosition
-            // 
-            this.savePosition.Location = new System.Drawing.Point(96, 113);
-            this.savePosition.Name = "savePosition";
-            this.savePosition.Size = new System.Drawing.Size(75, 23);
-            this.savePosition.TabIndex = 9;
-            this.savePosition.Text = "Guardar";
-            this.savePosition.UseVisualStyleBackColor = true;
-            this.savePosition.Click += new System.EventHandler(this.savePosition_Click);
-            // 
-            // cancelPosition
-            // 
-            this.cancelPosition.Location = new System.Drawing.Point(15, 113);
-            this.cancelPosition.Name = "cancelPosition";
-            this.cancelPosition.Size = new System.Drawing.Size(75, 23);
-            this.cancelPosition.TabIndex = 10;
-            this.cancelPosition.Text = "Cancelar";
-            this.cancelPosition.UseVisualStyleBackColor = true;
-            // 
             // positionsDataGrid
             // 
+            this.positionsDataGrid.AllowUserToAddRows = false;
+            this.positionsDataGrid.AllowUserToDeleteRows = false;
             this.positionsDataGrid.AutoGenerateColumns = false;
             this.positionsDataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.positionsDataGrid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
@@ -169,11 +144,64 @@
             this.minSalaryDataGridViewTextBoxColumn,
             this.maxSalaryDataGridViewTextBoxColumn});
             this.positionsDataGrid.DataSource = this.positionsBindingSource;
-            this.positionsDataGrid.Location = new System.Drawing.Point(12, 170);
+            this.positionsDataGrid.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnEnter;
+            this.positionsDataGrid.Location = new System.Drawing.Point(12, 153);
             this.positionsDataGrid.Name = "positionsDataGrid";
-            this.positionsDataGrid.Size = new System.Drawing.Size(523, 150);
+            this.positionsDataGrid.Size = new System.Drawing.Size(523, 167);
             this.positionsDataGrid.TabIndex = 0;
-            this.positionsDataGrid.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.positionsDataGrid.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.positionsDataGrid_CellValueChanged);
+            this.positionsDataGrid.DataError += new System.Windows.Forms.DataGridViewDataErrorEventHandler(this.positionsDataGrid_DataError);
+            // 
+            // cmbRiesgo
+            // 
+            this.cmbRiesgo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbRiesgo.FormattingEnabled = true;
+            this.cmbRiesgo.Items.AddRange(new object[] {
+            "Alto",
+            "Medio",
+            "Bajo"});
+            this.cmbRiesgo.Location = new System.Drawing.Point(104, 73);
+            this.cmbRiesgo.Name = "cmbRiesgo";
+            this.cmbRiesgo.Size = new System.Drawing.Size(121, 21);
+            this.cmbRiesgo.TabIndex = 11;
+            // 
+            // savePosition
+            // 
+            this.savePosition.Location = new System.Drawing.Point(15, 115);
+            this.savePosition.Name = "savePosition";
+            this.savePosition.Size = new System.Drawing.Size(75, 23);
+            this.savePosition.TabIndex = 9;
+            this.savePosition.Text = "Guardar";
+            this.savePosition.UseVisualStyleBackColor = true;
+            this.savePosition.Click += new System.EventHandler(this.savePosition_Click);
+            // 
+            // positionsTableAdapter
+            // 
+            this.positionsTableAdapter.ClearBeforeFill = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Nomina.Properties.Resources.iconPuesto;
+            this.pictureBox1.Location = new System.Drawing.Point(440, 31);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 91);
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            // 
+            // _payroll_unapecDataSet1
+            // 
+            this._payroll_unapecDataSet1.DataSetName = "_payroll_unapecDataSet";
+            this._payroll_unapecDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(104, 115);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.TabIndex = 13;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
             // 
             // idDataGridViewTextBoxColumn
             // 
@@ -194,7 +222,13 @@
             // 
             this.riskLevelDataGridViewTextBoxColumn.DataPropertyName = "RiskLevel";
             this.riskLevelDataGridViewTextBoxColumn.HeaderText = "Riesgo";
+            this.riskLevelDataGridViewTextBoxColumn.Items.AddRange(new object[] {
+            "Alto",
+            "Medio",
+            "Bajo"});
             this.riskLevelDataGridViewTextBoxColumn.Name = "riskLevelDataGridViewTextBoxColumn";
+            this.riskLevelDataGridViewTextBoxColumn.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.riskLevelDataGridViewTextBoxColumn.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
             this.riskLevelDataGridViewTextBoxColumn.Width = 80;
             // 
             // minSalaryDataGridViewTextBoxColumn
@@ -214,14 +248,15 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(552, 332);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.cmbRiesgo);
             this.Controls.Add(this.positionsDataGrid);
-            this.Controls.Add(this.cancelPosition);
             this.Controls.Add(this.savePosition);
             this.Controls.Add(this.positionMaxSalary);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.positionMinSalary);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.positionRisk);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.positionName);
             this.Controls.Add(this.label1);
@@ -232,6 +267,8 @@
             ((System.ComponentModel.ISupportInitialize)(this.payrollunapecDataSetBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this._payroll_unapecDataSet)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.positionsDataGrid)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this._payroll_unapecDataSet1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -245,17 +282,19 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox positionName;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox positionRisk;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox positionMinSalary;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.TextBox positionMaxSalary;
         private System.Windows.Forms.Button savePosition;
-        private System.Windows.Forms.Button cancelPosition;
         private System.Windows.Forms.DataGridView positionsDataGrid;
+        private System.Windows.Forms.ComboBox cmbRiesgo;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private _payroll_unapecDataSet _payroll_unapecDataSet1;
+        private System.Windows.Forms.Button btnEliminar;
         private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn riskLevelDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewComboBoxColumn riskLevelDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn minSalaryDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn maxSalaryDataGridViewTextBoxColumn;
     }
--- Nomina/PositionForm.cs
+++ Nomina/PositionForm.cs
@@ -26,47 +26,46 @@ namespace Nomina
         private void updateTableAdapter()
         {
             this.positionsTableAdapter.Fill(this._payroll_unapecDataSet.Positions);
-
-        }
-
-        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
-
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
         }
 
         private void savePosition_Click(object sender, EventArgs e)
         {
-            if(validateForm())
+            if (validateForm())
             {
-                using(var  dbContext = new PayrollDbContext())
+                using (var dbContext = new PayrollDbContext())
                 {
                     Position position = new Position();
                     position.Name = positionName.Text;
-                    position.RiskLevel = positionRisk.Text;
+                    position.RiskLevel = cmbRiesgo.SelectedItem.ToString();
                     position.MinSalary = double.Parse(positionMinSalary.Text);
                     position.MaxSalary = double.Parse(positionMaxSalary.Text);
 
                     dbContext.Positions.Add(position);
                     dbContext.SaveChanges();
                     updateTableAdapter();
+                    setDefaultValuesToInputs();
                 }
             }
         }
 
+        //Set default values to inputs on save item
+        private void setDefaultValuesToInputs()
+        {
+            positionName.Text = "";
+            positionMinSalary.Text = "";
+            positionMaxSalary.Text = "";
+            cmbRiesgo.SelectedIndex = -1;
+        }
+
         private bool validateForm()
         {
-            return validateName() && validateRisk() && validateRisk();
+            return validateName() && validateRisk() && validateSalaries();
         }
 
         private bool validateName()
         {
-            bool isValid = ! string.IsNullOrWhiteSpace(positionName.Text);
-            if(!isValid)
+            bool isValid = !string.IsNullOrWhiteSpace(positionName.Text);
+            if (!isValid)
             {
                 MessageBox.Show("Por favor valide nombre introducido.");
             }
@@ -74,13 +73,14 @@ namespace Nomina
             return isValid;
         }
 
-        //TODO: El riesgo seran constantes, texto libre o que??
         private bool validateRisk()
         {
-            bool isValid = !string.IsNullOrWhiteSpace(positionRisk.Text);
-            if(!isValid)
+            int selectedIndex = cmbRiesgo.SelectedIndex;
+            bool isValid = selectedIndex > 0;
+
+            if (!isValid)
             {
-                MessageBox.Show("Por favor valide el riesgo introducido.");
+                MessageBox.Show("Por favor seleccione un riesgo.");
             }
 
             return isValid;
@@ -91,21 +91,130 @@ namespace Nomina
             double minSalary, maxSalary = 0;
             bool isValid = false;
 
-            if(double.TryParse( positionMinSalary.Text, out minSalary ) 
+            if (double.TryParse(positionMinSalary.Text, out minSalary)
                 && double.TryParse(positionMaxSalary.Text, out maxSalary))
             {
                 isValid = minSalary <= maxSalary;
-                if(!isValid)
+                if (!isValid)
                 {
-                    MessageBox.Show("El salario minimo no puede ser mayor que el salario maximo.");
+                    MessageBox.Show("El salario mínimo no puede ser mayor que el salario máximo.");
                 }
             }
             else
             {
-                MessageBox.Show("Por favor valide los salarios introducidos.");
+                MessageBox.Show("Por favor valide los salarios introducidos. No se permiten letras ni signos.");
             }
 
             return isValid;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            //validate if any row has been selected
+            if (positionsDataGrid.SelectedRows.Count > 0)
+            {
+                int positionID = 0;
+                foreach (DataGridViewRow selectedRow in positionsDataGrid.SelectedRows)
+                {
+                    //confirm for delete position
+                    var confirmResult = MessageBox.Show("Está seguro que desea eliminar el/los puestos seleccionados?", "Confirmar Eliminación", MessageBoxButtons.YesNo);
+                    if (confirmResult == DialogResult.Yes)
+                    {
+                        //try to parse the column ID value to int
+                        if (int.TryParse(selectedRow.Cells[0].Value.ToString(), out positionID))
+                        {
+                            using (var dbContext = new PayrollDbContext())
+                            {
+                                //delete selected positions
+                                var currentPosition = dbContext.Positions.Where(p => p.Id == positionID).FirstOrDefault();
+                                if (currentPosition != null)
+                                {
+                                    dbContext.Positions.Remove(currentPosition);
+                                    dbContext.SaveChanges();
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        //exit if no confirm for delete position
+                        return;
+                    }
+                }
+
+                MessageBox.Show("Los puestos seleccionados fueron eliminados.");
+                updateTableAdapter();
+            }
+            else
+            {
+                MessageBox.Show("Debe seleccionar al menos una fila a eliminar.");
+            }
+        }
+
+        private void positionsDataGrid_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            //validate that current row is valid
+            if (e.RowIndex > -1)
+            {
+                int positionID;
+                //get id of position and try to parse it
+                if (int.TryParse(positionsDataGrid.Rows[e.RowIndex].Cells[0].Value.ToString(), out positionID))
+                {
+                    using (var dbContext = new PayrollDbContext())
+                    {
+                        var positionToUpdate = dbContext.Positions.Where(p => p.Id == positionID).FirstOrDefault();
+                        if (positionToUpdate != null)
+                        {
+                            //update attributes of position
+                            switch (e.ColumnIndex)
+                            {
+                                case 1:
+                                    string newName = positionsDataGrid.Rows[e.RowIndex].Cells[1].Value.ToString();
+                                    positionToUpdate.Name = newName;
+                                    dbContext.SaveChanges();
+                                    break;
+
+                                case 2:
+                                    string newRiskLevel = positionsDataGrid.Rows[e.RowIndex].Cells[2].Value.ToString();
+                                    positionToUpdate.RiskLevel = newRiskLevel;
+                                    dbContext.SaveChanges();
+                                    break;
+
+                                case 3:
+                                    int newMinSalaxy;
+                                    if (int.TryParse(positionsDataGrid.Rows[e.RowIndex].Cells[3].Value.ToString(), out newMinSalaxy))
+                                    {
+                                        positionToUpdate.MinSalary = newMinSalaxy;
+                                        dbContext.SaveChanges();
+                                    }
+                                    break;
+
+                                case 4:
+                                    int newMaxSalary;
+                                    if (int.TryParse(positionsDataGrid.Rows[e.RowIndex].Cells[4].Value.ToString(), out newMaxSalary))
+                                    {
+                                        positionToUpdate.MaxSalary = newMaxSalary;
+                                        dbContext.SaveChanges();
+                                    }
+                                    break;
+                            }
+                        }
+                    }
+                }
+                
+                MessageBox.Show("Puesto actualizado satisfactoriamente.");
+            }
+        }
+
+        //Message to show when validations error
+        private void positionsDataGrid_DataError(object sender, DataGridViewDataErrorEventArgs e)
+        {
+            MessageBox.Show("Inserte valores válidos, los salarios deben ser números.");
         }
     }
 }
