aa508069330690d572ad32a89d560d9bfbfdc092
Ruben Jimenez <ruben_ajp@hotmail.com>
2015-06-07 19:35:17 -0400

    Calculadora de AFP y SFS

---
+++ Nomina/AFPCalculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nomina
+{
+    class AFPCalculator
+    {
+        private const double FACTOR = 0.0287;
+
+        public static double getAFP(double salary)
+        {
+            return salary * FACTOR;
+        }
+    }
+}
---
+++ Nomina/SFSCalculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nomina
+{
+    class SFSCalculator
+    {
+        private const double FACTOR = 0.0304;
+
+        public static double getSFS(double salary)
+        {
+            return salary * FACTOR;
+        }
+    }
+}
