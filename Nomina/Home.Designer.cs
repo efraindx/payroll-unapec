a0121abeeaad5a94cad97b817c6b41513a022e83
Ruben Jimenez <ruben_ajp@hotmail.com>
2015-06-07 17:25:27 -0400

    Home desginer

--- Nomina/Home.Designer.cs
+++ Nomina/Home.Designer.cs
@@ -65,7 +65,7 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(145, 145);
             this.button2.TabIndex = 2;
-            this.button2.Text = "Tipos de Ingresos";
+            this.button2.Text = "Tipos Ingresos";
             this.button2.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
             this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
